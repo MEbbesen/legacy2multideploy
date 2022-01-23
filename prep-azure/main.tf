b11c1ddb0bbd64690c2e657a280fbe7fd95293d0
Mark Philip Mosskov Ebbesen <mark.ebbesen@gmail.com>
2022-01-23 16:00:36 +0100

    add terraform for azure resources create build pipeline

---
+++ prep-azure/main.tf
@@ -0,0 +1,33 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_location
+
+  tags = {
+    Environment = "Test"
+    Team        = "DevOps"
+  }
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.container_registry_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
